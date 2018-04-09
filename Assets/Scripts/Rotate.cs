e3f267230ef24065d793843cc2e2456cdb061471
Sander Koning <skoning@onenetbeyond.org>
2018-04-09 16:08:17 +0200

    Version 0.1

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float rotSpeed = 1.0f;
+
+    private Transform thisObj;
+
+    void Start()
+    {
+        // Cachhe the transform link
+        thisObj = this.transform;
+    }
+
+    void Update()
+    {
+        thisObj.Rotate(0, Time.deltaTime * rotSpeed, 0);
+    }
+
+}
